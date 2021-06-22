af68c076cab0e1d4b388c1154217154661732ee9
yzchyx <yuxuan.chen@fu-berlin.de>
2021-06-23 00:34:08 +0200

    integrate contrastive pre-training settings

--- evaluate.py
+++ evaluate.py
@@ -14,7 +14,9 @@ def evaluate(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
     evaluation_results = evaluate_config(cfg)
+
     logger.info("Evaluation results:\n%s" % evaluation_results)
+
     with open("./evaluation_results.json", "w") as f:
         json.dump(evaluation_results, f)
 
